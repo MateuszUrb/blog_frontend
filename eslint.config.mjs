82bd562fa256a4e7b401bccd9bba3a13192b7adf
Mateusz Urban <98urban.mateusz@gmail.com>
2025-03-07 17:49:04 +0100

    chore: updated blog types, tsconfgig, eslint,  added prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,14 @@
-import js from "@eslint/js";
-import globals from "globals";
-import jsdoc from "eslint-plugin-jsdoc";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import vitest from "@vitest/eslint-plugin";
-import reactRefresh from "eslint-plugin-react-refresh";
-import eslintConfigPrettier from "eslint-config-prettier";
+import js from "@eslint/js"
+import globals from "globals"
+import jsdoc from "eslint-plugin-jsdoc"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import vitest from "@vitest/eslint-plugin"
+import reactRefresh from "eslint-plugin-react-refresh"
+import eslintConfigPrettier from "eslint-config-prettier"
 
 export default [
   jsdoc.configs["flat/recommended"],
-  jsdoc.configs["flat/recommended-typescript-flavor"],
   eslintConfigPrettier,
   {
     ignores: ["dist", "node_modules", ".eslint.config.mjs", "vite.config.js"],
@@ -45,6 +44,10 @@ export default [
       ...reactHooks.configs.recommended.rules,
       "no-unused-vars": "off",
       "react/jsx-no-target-blank": "off",
+      "jsdoc/require-param-description": "off",
+      "jsdoc/require-returns-description": "off",
+      "jsdoc/require-returns-declaration": "off",
+      "jsdoc/require-returns": "off",
       "react/prop-types": "off",
       "react-refresh/only-export-components": [
         "warn",
@@ -52,4 +55,4 @@ export default [
       ],
     },
   },
-];
+]
--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,14 +1,20 @@
 export type BlogProps = {
-  title: string;
-  author: string;
-  likes?: number;
-  url: string;
-  users: UserProps[];
-  id: string;
-};
+  title: string
+  author: string
+  likes: number
+  url: string
+  users: UserProps[]
+  id: string
+}
 
 export type UserProps = {
-  token: string;
-  username: string;
-  name: string;
-};
+  token: string
+  username: string
+  name: string
+}
+
+export type BlogInfo = {
+  title: string
+  author: string
+  url: string
+}
